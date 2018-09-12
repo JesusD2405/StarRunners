3e9a13016123ecd1357b5160cb07a0ba61f6811a
Miguel toledo <migueltoledo046@gmail.com>
2018-09-12 18:35:10 -0400

    js de cada display

--- StarRunners/settings.py
+++ StarRunners/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'StarRunners',
         'USER': 'root',
-        'PASSWORD': 'mysql',
+        'PASSWORD': 'Pjtg.2009',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
